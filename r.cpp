9bdf41ee9495043c24e52792832d4c6234832dc4
roa0mar <roa0mar999@gmail.com>
2024-11-21 18:57:36 +0300

    update

---
+++ r.cpp
@@ -0,0 +1,13 @@
+Steps to Connect a Local Repository to GitHub:
+
+1. Creat a GitHub Repository
+2. Initialize a Git repository (command git init)
+3. Add files (command git add)
+4. Save the changes to the local repository (command git commit)
+5. link the local repository to GitHub:
+   A- copy repository URL from gitHub
+   B- git remote add origin repository URL
+
+GitHub Repository Link :-
+
+https://github.com/roa0mar/AlgoCourse.git
